79e8838ca97aac6c8886030bced202eb2f0dd0c6
Alfredo Rodriguez <arodrigu12@hotmail.com>
2020-02-04 19:35:15 -0500

    How much candy to buy

---
+++ HowMuchCandyToBuy/HowMuchCandyToBuy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HowMuchCandyToBuy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double candy = 1.1 * 250 * 3;
+
+            Console.WriteLine("If weather clear and in the 50's you buy " + candy.ToString());
+
+            candy = 1.2 * 250 * 3;
+
+            Console.WriteLine("If full moon and in the 40s you buy " + candy.ToString());
+
+            candy = .95 * 250 * 3;
+
+            Console.WriteLine("rainy and in the 70s you buy " + candy.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
